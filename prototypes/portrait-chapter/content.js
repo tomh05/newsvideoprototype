7e87cede41e3781588e9cc46b59d38b37c14dea2
Tom Howe <tom.howe@bbc.co.uk>
2015-03-02 18:25:15 +0000

    chapter prototype

--- prototypes/portrait-chapter/content.js
+++ prototypes/portrait-chapter/content.js
@@ -1,38 +1,26 @@
 var title = "Malaysia jet crashes in east Ukraine conflict zone";
 
-var chapters = { 
-"0":{"start":"0","end":"15","captions":[
-    {"start":"0","end":"5","body":""  }, 
-        {"start":"5","end":"9","body":""  }, 
-        ]}, 
-"1":{"start":"15","end":"33","captions":[
-    {"start":"0","end":"9","body":""  }, 
-    {"start":"0","end":"9","body":""  }, 
+var chapters = [ 
+{"start":"0","end":"15","captions":[
+    {"start":"0","end":"5","body":"A Malaysia Airlines jet carrying 295 people has crashed in east Ukraine on a flight from Amsterdam to Kuala Lumpur."  }, 
+        {"start":"5","end":"9","body":"Flight MH17 had been due to enter <a href='civil'>Russian airspace</a> when contact was lost."  }, 
+        {"start":"9","end":"15","body":"US and Ukrainian officials said they believed the plane had been brought down by a missile."  }, 
+        ]},
+{"start":"15","end":"33","captions":[
+    {"start":"0","end":"6","body":"The <a href='region'>crash site</a> is in an area controlled by Ukraine's separatist rebels"  }, 
+    {"start":"6","end":"12","body":"At least <a href='passengers'>100 bodies</a> have been found so far at the scene"},
+    {"start":"12","end":"17","body":"with wreckage spread across an area of up to about 15km (nine miles) in diameter."},
     ]}, 
-"2":{"start":"33","end":"46","captions":[
-    {"start":"0","end":"9","body":""  }, 
-    {"start":"0","end":"9","body":""  }, 
+{"start":"33","end":"46","captions":[
+    {"start":"0","end":"6","body":"Foreign Minister Pavlo Klimkin told the BBC he had intercepted phone conversations that proved the <a href='missile'>plane was shot down</a> by pro-Russian separatists."  }, 
+    {"start":"6","end":"12","body":"But Russian President Vladimir Putin blamed the Ukraine government for restarting military operations in the area, where it is trying to regain control from pro-Russian rebels."  }, 
+    //{"start":"0","end":"9","body":"Ukrainian President Petro Poroshenko said it was an \"act of terrorism\""  }, 
     ]}, 
-};
+];
 
-    var captions = [ 
-{"start":"0","end":"9","body":"A Malaysia Airlines jet carrying 295 people has crashed in east Ukraine on a flight from Amsterdam to Kuala Lumpur."},
-{"start":"9","end":"15","body":"There are no signs of survivors at the scene of the crash near the village of Grabovo, in rebel-held territory close to the border with Russia."},
-{"start":"15","end":"25","body":"Both sides in Ukraine's <a href='civil'> civil conflict</a> accused each other of shooting down the plane with a missile. It is still not clear why the plane came down."},
-{"start":"25","end":"35","body":"The plane fell between Krasni Luch in <a href='region'>Luhansk region</a> and Shakhtarsk in the neighbouring region of Donetsk."},
-{"start":"35","end":"40","body":"The crash site is in an area controlled by Ukraine's separatist rebels."},
-{"start":"40","end":"50","body":"At least <a href='passengers'>100 bodies</a> have been found so far at the scene, an emergency services worker told Reuters news agency"},
-{"start":"50","end":"55","body":"with wreckage spread across an area of up to about 15km (nine miles) in diameter."},
-{"start":"55","end":"60","body":"US and Ukrainian officials said they believed the plane had been <a href='missile'>brought down by a missile</a>."},
-{"start":"60","end":"68","body":"Ukrainian President Petro Poroshenko said it was an 'act of terrorism'."},
-
-
-    ];
-
-
-    var overlays = {
+var overlays = {
         "civil":"<h2>Civil Conflict</h2><p>The crisis in Ukraine began in November last year when pro-Moscow President Viktor Yanukovych abandoned a deal with the EU in favour of stronger ties with Russia.</p><p> Protests erupted in the capital Kiev and quickly escalated as government buildings were seized in cities across the western regions of Ukraine.</p>",
-        "region":"<h2>Crash Site Location</h2><img src='http://news.bbcimg.co.uk/media/images/76339000/gif/_76339189_ukraine_malaysia_plane_976_latest.gif' width='250' height='180' />",
+        "region":"<h2>Crash Site Location</h2><img src='../../articles/ukraine-flight/crashsite.gif' width='250' height='180' />",
         "passengers":"<h2>Passengers</h2> <p>At a news conference at Schiphol airport, Malaysia Airlines' European chief Huib Gorter said they were still trying to identify some of the passengers from flight MH17. <p>He said of the passengers that have been identified there were:</p> <ul><li>154 Dutch nationals</li><li>27 Australians</li><li> 23 Malaysians</li><li> 11 Indonesians</li><li> Six Britons</li><li> four Germans</li><li> four Belgians</li><li> three from the Philippines</li><li>one Canadian.</ul><p>All 15 of the crew were Malaysian.</p>",
-        "missile":"<h2>Analysis: Jonathan Marcus, BBC News</h2> <p>If it does turn out that the Boeing 777 was shot down by the separatists - with weaponry supplied by Moscow - then it could significantly alter the terms of the whole debate surrounding the Ukraine crisis.</p><p>Over the past few days there has been growing concern among Western governments that <p>Russia was stepping up its military support for the separatists in eastern Ukraine.</p><p>Nato spokesmen insist that more and more heavy military equipment has moved from Russian stockpiles to the separatists across the border.</p>",
+        "missile":"<h2>Analysis: Jonathan Marcus, BBC News</h2> <p>If it does turn out that the Boeing 777 was shot down by the separatists - with weaponry supplied by Moscow - then it could significantly alter the terms of the whole debate surrounding the Ukraine crisis.</p><p>Over the past few days there has been growing concern among Western governments that Russia was stepping up its military support for the separatists in eastern Ukraine.</p><p>Nato spokesmen insist that more and more heavy military equipment has moved from Russian stockpiles to the separatists across the border.</p>",
     };
--- prototypes/portrait-chapter/vidplayer.js
+++ prototypes/portrait-chapter/vidplayer.js
@@ -1,88 +1,83 @@
-function getUrlParameter(sParam) {
-    var sPageURL = window.location.search.substring(1);
-    var sURLVariables = sPageURL.split('&');
-    for (var i = 0; i < sURLVariables.length; i++) 
-    {
-        var sParameterName = sURLVariables[i].split('=');
-        if (sParameterName[0] == sParam) 
-        {
-            return sParameterName[1];
+$( document ).ready(function() {
+    for (var i=0;i<chapters.length;i++) {
+        $("#chapterIndicator ul").append("<li></li>");
+    }
+    $("#chapterIndicator li").width( (300/chapters.length) + "px");
+    $("#chapterIndicator li:eq(0)").addClass("chapterCurrent");
+
+
+    var currentChapter = 0;
+    function getUrlParameter(sParam) {
+        var sPageURL = window.location.search.substring(1);
+        var sURLVariables = sPageURL.split('&');
+        for (var i = 0; i < sURLVariables.length; i++) {
+            var sParameterName = sURLVariables[i].split('=');
+            if (sParameterName[0] == sParam) {
+                return sParameterName[1];
+            }
+        }
+    }          
+
+    // "swipe up to see more" indicator toggling
+    function updateIndicator() {
+        var key = $("#caption").find('a').attr("href");
+        console.log(key);
+        if (key == undefined) {
+            //$("#contentindicator").fadeOut();
+        } else {
+            //$("#contentindicator").fadeIn();
         }
     }
-}          
 
-function updateIndicator() {
-    var key = $("#caption").find('a').attr("href");
-    console.log(key);
-    if (key == undefined) {
-        $("#contentindicator").fadeOut();
-    } else {
-        $("#contentindicator").fadeIn();
-    }
-}
-function CaptionState(capdata){
-    this.currentIndex=-1;
-    this.manualmode=false;
+    function nextChapter() {
+        console.log("next chapter");
+        if (currentChapter < chapters.length -1){
+            //$("#chapterIndicator li:eq("+currentChapter+")").removeClass("chapterCurrent");
+            //currentChapter ++;
+            //$("#chapterIndicator li:eq("+currentChapter+")").addClass("chapterCurrent");
+            //pop.currentTime(chapters[currentChapter].start);
+;
 
-    this.advance = function(animate) {
-        if (this.currentIndex >= capdata.length-1) return;
-        this.manualmode = true;
-        this.currentIndex +=1;
-        if (!animate) {
-            console.log("caption is now "+this.currentIndex);
-            $("#caption").html(capdata[this.currentIndex].body);
-            updateIndicator();
-        } else {
-            console.log("caption is now "+this.currentIndex);
-            $("#caption").after('<div id="nextcaption">'+ capdata[this.currentIndex].body + '</div>');
+            $("#caption").after('<div id="nextcaption">'+ chapters[currentChapter+1].captions[0].body+ '</div>');
             $("#nextcaption").animate({left: "0"},{duration: 200, queue: false});
             $("#caption").animate({left: "-300"},{duration: 200, queue: false, complete: function() {
                 $("#caption").remove();
                 $("#nextcaption").attr('id','caption');
                 updateIndicator();
+            var video = document.getElementsByTagName("video")[0];
+            video.currentTime = chapters[currentChapter+1].start
             }});
+
+
+
         }
-
     }
 
-    this.goBack = function() {
-        if (this.currentIndex<=0) return;
-        this.manualmode = true;
-        console.log("back")
-            this.currentIndex -=1;
+    function prevChapter() {
+        console.log("prev chapter");
+        console.log("current chapter" + currentChapter);
+        if (currentChapter > 0){
+            //$("#chapterIndicator li:eq("+currentChapter+")").removeClass("chapterCurrent");
+            //currentChapter --;
+            //$("#chapterIndicator li:eq("+currentChapter+")").addClass("chapterCurrent");
+            //pop.currentTime(chapters[currentChapter].start);
+            $("#caption").before('<div id="prevcaption">'+chapters[currentChapter-1].captions[0].body+ '</div>');
+            console.log("body " + chapters[currentChapter-1].captions[0].body);
+            $("#prevcaption").animate({left: "0"},{duration: 200, queue: false});
+            $("#caption").animate({left: "300"},{duration: 200, queue: false, complete: function() {
+                $("#caption").remove();
+                $("#prevcaption").attr('id','caption');
+                updateIndicator();
+            var video = document.getElementsByTagName("video")[0];
+            video.currentTime = chapters[currentChapter-1].start;
 
-        console.log("caption is now "+this.currentIndex);
-        $("#caption").before('<div id="prevcaption">'+ capdata[this.currentIndex].body + '</div>');
-        $("#prevcaption").animate({left: "0"},{duration: 200, queue: false});
-        $("#caption").animate({left: "300"},{duration: 200, queue: false, complete: function() {
-            $("#caption").remove();
-            $("#prevcaption").attr('id','caption');
-            updateIndicator();
-        }});
-    }
 
-    this.setIndexFromVideo = function(n) {
-        if (n < 0 || n >= capdata.length) return;
+            }});
 
-
-        if (!this.manualmode){
-            console.log("auto-switching to caption "+n);
-            this.currentIndex = n;
-
-            $("#caption").html(capdata[n].body);
-        } else {
-            // if video is back in sync with captions, continue
-            if (n == this.currentIndex) {
-                console.log("returning to automatic");
-                this.manualmode = false;
-            }
         }
-        updateIndicator();
     }
-}
 
 
-$( document ).ready(function() {
     // Load article media
     //$.getScript('../../articles/'+ getUrlParameter("article")+'/content.js',function() {
     $('#title h1').html(title);
@@ -92,7 +87,7 @@ $( document ).ready(function() {
     var pop = Popcorn("#video");
 
     console.log("creating video");
-    var captionState = new CaptionState(captions);
+    //var captionState = new CaptionState(captions);
 
     // define popcorn triggers - chapters and captions
     (function ( Popcorn ) {
@@ -100,22 +95,21 @@ $( document ).ready(function() {
             _setup: function( options ) {
                 var target = document.getElementById( options.target );
             },
-
             start: function( event, options ){
                 console.log("caption start");
-                $( options._container ).fadeIn(); // <---
-                captionState.setIndexFromVideo(options.id);
+                //$( options._container ).fadeIn(); // <---
+                //captionState.setIndexFromVideo(options.id);
+                console.log(options);
+                $("#caption").html(options.text);
             },
-
             end: function( event, options ){
                 console.log("caption end");
-                $( options._container ).fadeOut(); // <---
+                //$( options._container ).fadeOut(); // <---
             },
             _teardown: function( options ) {
                 document.getElementById( options.target ) && document.getElementById( options.target ).removeChild( options._container );
             }
         });
-
     })( Popcorn );
 
     (function ( Popcorn ) {
@@ -123,68 +117,60 @@ $( document ).ready(function() {
             _setup: function( options ) {
                 var target = document.getElementById( options.target );
             },
-
             start: function( event, options ){
                 console.log("chapter start");
-                for (var i=0; i< captions.length;i++) {
-                    var a = pop.caption({
-                        start: captions[i].start,
-                        end: captions[i].end,
-                        text: captions[i].body,
-                        id: i,
-                        target: "caption"
-                    });
-                }
+            currentChapter = options.id;
+            $("#chapterIndicator li").removeClass("chapterCurrent");
+            $("#chapterIndicator li:eq("+options.id+")").addClass("chapterCurrent");
 
             },
-
             end: function( event, options ){
                 console.log("chapter end");
-                pop.pause();
             },
             _teardown: function( options ) {
                 document.getElementById( options.target ) && document.getElementById( options.target ).removeChild( options._container );
             }
         });
-
     })( Popcorn );
 
     // populate chapters
     console.log(chapters);
-    $.each(chapters,function(index,chap) {
-        //for (var i=0; i< chapters.length;i++) {
+    for (var i=0; i< chapters.length;i++) {
         console.log("creating chapter");
+        var chap = chapters[i];
         var a = pop.chapter({
             start: chap.start,
             end: chap.end,
-            captions: chap.captions,
-            id: index,
-            //target: "caption"
+            //captions: chap.captions,
+            id: i,
         });
-    });
-
-    /*
-    // populate captions
-    for (var i=0; i< captions.length;i++) {
-    var a = pop.footnoteCustom({
-    start: captions[i].start,
-    end: captions[i].end,
-    text: captions[i].body,
-    id: i,
-    target: "caption"
-    });
-    }
-    */
-
-
+        // populate captions
+        var caps = chapters[i].captions;
+        for (var j=0; j< caps.length;j++) {
+            var a = pop.caption({
+                start: parseInt(chapters[i].start,10) + parseInt(caps[j].start,10),
+                end: parseInt(chapters[i].start,10) + parseInt(caps[j].end,10),
+                text: caps[j].body,
+                id: j,
+                target: "caption"
+            });
+        }
+    };
 
     // play the video
     pop.play();
 
-
     // clicking on caption links
     $(document).on("click","#caption a", function(e) {
         e.preventDefault();
+        var key = $("#caption").find('a').attr("href");
+        console.log(key);
+        if (key !== undefined) {
+            console.log("it isnt undefined");
+            $("#overlaycontent").html(overlays[key]);
+            $("#overlay").fadeIn();
+            pop.pause();
+        }
     });
 
     // exit overlay
@@ -194,34 +180,65 @@ $( document ).ready(function() {
     });
 
     // tap a caption to skip to next one
-    $(document).on("tap", "#caption",function(event){
-        console.log("tap");
-        captionState.advance(false);
-    });
+    /*
+       $(document).on("tap", "#caption",function(event){
+       console.log("tap");
+       captionState.advance(false);
+       });
+       */
+    /*
     // swipe a caption to go back next one
-
-    //$(document).on("swiperight", "#caption",function(event){
     $("#captionholder").swipe({
-        swipeRight:function(event, direction, distance, duration, fingerCount){
-            console.log("swipe");
-            captionState.goBack();
-        },
-        swipeLeft:function(event, direction, distance, duration, fingerCount){
-            console.log("swipe");
-            captionState.advance(true);
-        },
-        swipeUp:function(event, direction, distance, duration, fingerCount){
-            console.log("up");
-            var key = $("#caption").find('a').attr("href");
-            console.log(key);
-            if (key !== undefined) {
-                console.log("it isnt undefined");
-                $("#overlaycontent").html(overlays[key]);
-                $("#overlay").fadeIn();
-                pop.pause();
-            }
-        },
-        //Default is 75px, set to 0 for demo so any distance triggers swipe
-        threshold:50
+    swipeRight:function(event, direction, distance, duration, fingerCount){
+    console.log("swipe");
+    captionState.goBack();
+    },
+    swipeLeft:function(event, direction, distance, duration, fingerCount){
+    console.log("swipe");
+    captionState.advance(true);
+    },
+    swipeUp:function(event, direction, distance, duration, fingerCount){
+    console.log("up");
+    var key = $("#caption").find('a').attr("href");
+    console.log(key);
+    if (key !== undefined) {
+    console.log("it isnt undefined");
+    $("#overlaycontent").html(overlays[key]);
+    $("#overlay").fadeIn();
+    pop.pause();
+    }
+    },
+    //Default is 75px, set to 0 for demo so any distance triggers swipe
+    threshold:50
     });
+    */
+    $("#videodiv").swipe({
+        swipeRight:function(event, direction, distance, duration, fingerCount){
+            console.log("swipe right video");
+            prevChapter();
+        },
+    swipeLeft:function(event, direction, distance, duration, fingerCount){
+        console.log("swipe left video");
+        nextChapter();
+    },
+    threshold:50
+    });
+
+    $(document).on("tap", "#playblocker",function(event){
+        pop.play();
+        $(this).html("loading video...");
+    });
+
+    $('video').on('loadeddata',function(){
+        $("#playblocker").fadeOut();
+    });
+
+
+
+
+    $(document).on("tap", "#videodiv",function(event){
+        pop.play();
+
+    });
+
 });

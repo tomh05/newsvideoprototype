4551084e21f7bd7305c5eed82df9ffe1854286fb
Tom Howe <tom.howe@bbc.co.uk>
2015-03-04 16:28:51 +0000

    ui improvements

--- prototypes/portrait-chapter-3/vidplayer.js
+++ prototypes/portrait-chapter-3/vidplayer.js
@@ -1,14 +1,22 @@
 $( document ).ready(function() {
 
+    var replayButton=false;
+
     var pop = Popcorn("#video");
     var playVideo = function() {
         console.log("playing video");
         $("#paused").fadeOut();
         pop.play();
     }
-    var pauseVideo = function() {
+    var pauseVideo = function(replay) {
         console.log("pausing video");
         pop.pause();
+        replayButton = replay;
+        if (replay) { //show a replay arrow instead
+            $("#paused").html("&#8635;");
+        } else {
+            $("#paused").html("&#9658;");
+        }
         $("#paused").fadeIn();
     }
 
@@ -51,43 +59,48 @@ $( document ).ready(function() {
                 console.log("vidChapter");
                 pop.currentTime(chapters[newEl.id].start);
                 playVideo();
-                console.log("cueueing for "+chapters[newEl.id].end);
-                pop.cue(chapters[newEl.id].end-1,function(){
-                    pauseVideo();
-                });
+                console.log("will queue for "+n +" at "+chapters[newEl.id].end);
+                //pop.cue(chapters[newEl.id].end-1,this.endChapter(n));
                 $("#videoholder").show();
                 $("#richtextholder").hide();
 
             } else if (newEl.type=="richText") {
                 console.log("richText");
-                pauseVideo();
+                pauseVideo(false);
                 $("#richtextholder").html(richtexts[newEl.id].body);
                 $("#richtextholder").show();
                 $("#videoholder").hide();
             } 
         };
+        this.endChapter = function(chapterID) {
+            //only pause if video reached natural end
+            console.log("chapid "+chapterID + ", curel "+this.timeline.getAt(this.currentElement).id);
+            if (chapterID == this.timeline.getAt(this.currentElement).id) { 
+                    pauseVideo(true);
+            }
+        }
         this.nextElement = function() {
             if (this.currentElement < this.timeline.length()-1) {
-                this.goToElement(currentElement+1);
+                this.goToElement(this.currentElement+1);
             }
         };
         this.prevElement = function() {
             if (this.currentElement > 0) {
-                this.goToElement(currentElement-1);
+                this.goToElement(this.currentElement-1);
             }
         };
 
         this.populateTimeline = function() {
-        for (var i=0;i<this.timeline.length();i++) {
-            console.log("t "+this.timeline.getAt(i).type );
-            if (this.timeline.getAt(i).type == "vidChapter") {
-                var title = chapters[this.timeline.getAt(i).id].title;
-                $("#timelineDiv").append("<div class=\"timelineEl typeVid\">"+title+"</div>");
-            } else {
-                var title = richtexts[this.timeline.getAt(i).id].title;
-                $("#timelineDiv").append("<div class=\"timelineEl typeRich\">"+title+"</div>");
+            for (var i=0;i<this.timeline.length();i++) {
+                console.log("t "+this.timeline.getAt(i).type );
+                if (this.timeline.getAt(i).type == "vidChapter") {
+                    var title = chapters[this.timeline.getAt(i).id].title;
+                    $("#timelineDiv").append("<div class=\"timelineEl typeVid\"><div class=\"bg\"></div><div class=\"con\">"+title+"</div></div>");
+                } else {
+                    var title = richtexts[this.timeline.getAt(i).id].title;
+                    $("#timelineDiv").append("<div class=\"timelineEl typeRich\"><div class=\"bg\"></div><div class=\"con\">"+title+"</div></div>");
+                }
             }
-        }
             $("#timelineDiv .timelineEl:eq(0)").addClass("currentEl");
         }
 
@@ -99,6 +112,24 @@ $( document ).ready(function() {
             $("#timelineDiv .timelineEl:eq("+(this.currentElement+1)+")").animate({"max-width": "200px"},{duration:400,queue:false});
             this.timeline.addElementAt(at,type,id);
 
+        }
+
+        this.updateTimelineBackground = function() {
+            var curEl = this.timeline.getAt(this.currentElement);
+            if (curEl.type=="vidChapter") {
+                var t = pop.currentTime();
+                var s = chapters[curEl.id].start;
+                var e = chapters[curEl.id].end;
+                var percent = 100 * (t-s) / (e-s);
+            $("#timelineDiv .timelineEl:eq("+this.currentElement+") .bg").width(percent+"%");
+            }
+        }
+        this.restartVideoChapter = function() {
+            var el = this.timeline.getAt(this.currentElement);
+            if (el.type=="vidChapter") {
+            pop.currentTime(chapters[el.id].start);
+            playVideo();
+            }
         }
     };
 
@@ -124,30 +155,37 @@ $( document ).ready(function() {
     });
 
 
-    $("#videodiv").swipe({
+    $("#videoholder, #richtextholder").swipe({
         swipeRight:function(event, direction, distance, duration, fingerCount){
             console.log("swipe right video");
-            prevChapter();
+            //prevChapter();
+            controller.prevElement();
         },
-    swipeLeft:function(event, direction, distance, duration, fingerCount){
-        console.log("swipe left video");
-        nextChapter();
-    },
-    threshold:50
+        swipeLeft:function(event, direction, distance, duration, fingerCount){
+            console.log("swipe left video");
+            //nextChapter();
+            controller.nextElement();
+        },
+        threshold:50
     });
 
-   /*
+    //update background on video
+    pop.on("timeupdate",function() {
+        controller.updateTimelineBackground();
+    });
 
-    for (var i=0;i<chapters.length;i++) {
-        $("#chapterIndicator ul").append("<li>"+chapters[i].title+"</li>");
-    }
+    /*
+
+       for (var i=0;i<chapters.length;i++) {
+       $("#chapterIndicator ul").append("<li>"+chapters[i].title+"</li>");
+       }
     //$("#chapterIndicator li").width( (300/chapters.length) + "px");
     $("#chapterIndicator li:eq(0)").addClass("chapterCurrent");
 
 
     var currentChapter = 0;
 
-    */
+*/
     function getUrlParameter(sParam) {
         var sPageURL = window.location.search.substring(1);
         var sURLVariables = sPageURL.split('&');
@@ -167,8 +205,8 @@ $( document ).ready(function() {
             $("#caption").animate({left: "-300"},{duration: 200, queue: false, complete: function() {
                 $("#caption").remove();
                 $("#nextcaption").attr('id','caption');
-            var video = document.getElementsByTagName("video")[0];
-            video.currentTime = chapters[currentChapter+1].start
+                var video = document.getElementsByTagName("video")[0];
+                video.currentTime = chapters[currentChapter+1].start
             }});
         }
     }
@@ -183,8 +221,8 @@ $( document ).ready(function() {
             $("#caption").animate({left: "300"},{duration: 200, queue: false, complete: function() {
                 $("#caption").remove();
                 $("#prevcaption").attr('id','caption');
-            var video = document.getElementsByTagName("video")[0];
-            video.currentTime = chapters[currentChapter-1].start;
+                var video = document.getElementsByTagName("video")[0];
+                video.currentTime = chapters[currentChapter-1].start;
             }});
         }
     }
@@ -224,10 +262,11 @@ $( document ).ready(function() {
                 var target = document.getElementById( options.target );
             },
             start: function( event, options ){
-        
+
             },
             end: function( event, options ){
                 console.log("chapter end");
+                controller.endChapter(options.id);
             },
             _teardown: function( options ) {
                 document.getElementById( options.target ) && document.getElementById( options.target ).removeChild( options._container );
@@ -263,7 +302,12 @@ $( document ).ready(function() {
     pop.play();
 
     $(document).on("tap", "#videodiv",function(event){
+        if (replayButton) {
+            controller.restartVideoChapter();
+        } else {
         playVideo();
+        }
+        
     });
 
     var tappedToLoad=false;
@@ -277,5 +321,5 @@ $( document ).ready(function() {
         $("#playblocker").fadeOut();
         controller.goToElement(0);
     });
- 
+
 });
